cb6cecb7da816aa4bd4b5bb294157bd66d060843
Dima <dimjke-72@mail.ru>
2023-08-05 09:58:20 +0500

    lesson2 HW task1 added

---
+++ src/main/java/JavaExceptions/Lesson2/HomeWork/task1.java
@@ -0,0 +1,56 @@
+package JavaExceptions.Lesson2.HomeWork;
+
+import java.util.Scanner;
+
+/**
+ * Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+ * и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения,
+ * вместо этого, необходимо повторно запросить у пользователя ввод данных.
+ */
+public class task1 {
+    public static void main(String[] args) {
+        System.out.println("Введите число");
+        inHasNextFloat();
+        inNextFloat();
+    }
+
+    /**
+     * @apiNote проверка на float через HasFloat
+     */
+    public static void inHasNextFloat() {
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            System.out.print("->");
+            if (in.hasNextFloat()) {
+                System.out.println(in.nextFloat());
+                break;
+            }
+            System.out.println("не число ");
+        }
+    }
+
+    /**
+     * @apiNote проверка на float использую parseFloat
+     */
+    public static void inNextFloat() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("->");
+        String result = in.next();
+        while (!inFloat(result)) {
+            System.out.println("не число ");
+            System.out.print("->");
+            result = in.next();
+        }
+        System.out.println(result);
+    }
+
+    private static boolean inFloat(String num) {
+        try {
+            Float.parseFloat(num);
+            return true;
+        }
+        catch (NumberFormatException ex) {
+            return false;
+        }
+    }
+}
